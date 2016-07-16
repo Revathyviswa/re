c9dbd85cc7370a29b395534e6149eae49603bb12
Revathyviswa <hemarev05@gmail.com>
2016-07-16 17:30:04 +0530

    Add files via upload

---
+++ Even2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package even2;
+
+/**
+ *
+ * @author Student
+ */
+public class Even2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here for(int i=10;i<=20;i++)
+    for(int i=10;i<=20;i++)
+       {
+           if(i%2==1)
+           {
+               System.out.println(i);
+           }
+               }
+    }
+    }
+
